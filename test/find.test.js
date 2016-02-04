d6550f2c249b53d3ad952a5ccca2212ae21a9516
Zoujie <yibn2008@gmail.com>
2016-02-04 17:19:58 +0800

    fix coding style and remove unused deps

--- test/find.test.js
+++ test/find.test.js
@@ -2,16 +2,16 @@
 * @Author: zoujie.wzj
 * @Date:   2016-01-24 10:35:16
 * @Last Modified by:   Zoujie
-* @Last Modified time: 2016-01-24 15:51:37
+* @Last Modified time: 2016-02-04 17:17:21
 */
 
-'use strict';
+'use strict'
 
-const assert = require('assert');
-const path = require('path');
-const cp = require('child_process');
-const find = require('..');
-const listen = require('./fixtures/listen_port');
+const assert = require('assert')
+const path = require('path')
+const cp = require('child_process')
+const find = require('..')
+const listen = require('./fixtures/listen_port')
 
 describe('Find process test', function () {
   it('should find process of listenning port', function () {
@@ -19,50 +19,49 @@ describe('Find process test', function () {
       .then(function () {
         return find('port', 12345)
           .then(function (list) {
-            listen.close();
+            listen.close()
 
-            assert(list.length === 1);
-            assert.equal(process.pid, list[0].pid);
+            assert(list.length === 1)
+            assert.equal(process.pid, list[0].pid)
           }, function (err) {
-            listen.close();
+            listen.close()
 
-            assert(false, err.stack || err);
-          });
-      });
-  });
+            assert(false, err.stack || err)
+          })
+      })
+  })
 
   it('should find process of pid', function () {
-    let file = path.join(__dirname, 'fixtures/child_process.js');
-    let cps = cp.spawn(process.execPath, [file]);
+    let file = path.join(__dirname, 'fixtures/child_process.js')
+    let cps = cp.spawn(process.execPath, [file])
 
     return find('pid', cps.pid)
       .then(function (list) {
-        cps.kill();
+        cps.kill()
 
-        assert(list.length === 1);
-        assert.equal(cps.pid, list[0].pid);
+        assert(list.length === 1)
+        assert.equal(cps.pid, list[0].pid)
       }, function (err) {
-        cps.kill();
+        cps.kill()
 
-        assert(false, err.stack || err);
-      });
-  });
+        assert(false, err.stack || err)
+      })
+  })
 
   it('should find process list matched given name', function () {
-    let file = path.join(__dirname, 'fixtures/child_process.js');
-    let cps = cp.spawn(process.execPath, [file, 'AAABBBCCC']);
+    let file = path.join(__dirname, 'fixtures/child_process.js')
+    let cps = cp.spawn(process.execPath, [file, 'AAABBBCCC'])
 
     return find('name', 'AAABBBCCC')
       .then(function (list) {
-        cps.kill();
+        cps.kill()
 
-        assert(list.length === 1);
-        assert.equal(cps.pid, list[0].pid);
-
+        assert(list.length === 1)
+        assert.equal(cps.pid, list[0].pid)
       }, function (err) {
-        cps.kill();
+        cps.kill()
 
-        assert(false, err.stack || err);
-      });
-  });
-});
+        assert(false, err.stack || err)
+      })
+  })
+})
--- test/fixtures/listen_port.js
+++ test/fixtures/listen_port.js
@@ -2,29 +2,29 @@
 * @Author: zoujie.wzj
 * @Date:   2016-01-24 10:39:27
 * @Last Modified by:   Zoujie
-* @Last Modified time: 2016-01-24 15:44:14
+* @Last Modified time: 2016-02-04 17:17:32
 */
 
-'use strict';
+'use strict'
 
-const http = require('http');
+const http = require('http')
 
 let server = http.createServer(function () {
   // empty
-});
+})
 
 module.exports = function (port) {
   return new Promise((resolve, reject) => {
     server.listen(port, function (err) {
       if (err) {
-        reject(err);
+        reject(err)
       } else {
-        resolve();
+        resolve()
       }
-    });
-  });
-};
+    })
+  })
+}
 
 module.exports.close = function () {
-  server.close();
-};
+  server.close()
+}
